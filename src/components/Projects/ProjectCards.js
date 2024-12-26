800ae42b02b23cd5734c464a3830516846169801
Code9X <sebinsaji0809@gmail.com>
2024-12-26 16:30:33 +0530

    Modified Projects

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -3,6 +3,7 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
+import "./ProjectCard.css";
 
 function ProjectCards(props) {
   return (
@@ -10,8 +11,8 @@ function ProjectCards(props) {
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
       <Card.Body style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', flexGrow: 1 }}>
         <Card.Title>{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify", flexGrow: 1 }}>
-          {props.description}
+        <Card.Text className="description" style={{ flexGrow: 1 }}>
+        {props.description.trim()}
         </Card.Text>
         <div style={{ marginTop: "auto" }}>
           <Button variant="primary" href={props.ghLink} target="_blank">
