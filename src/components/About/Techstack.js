f5b51161cf5b48a035fc283caa82ea2cf4200a0c
Code9X <sebinsaji0809@gmail.com>
2025-06-02 23:17:54 +0530

    Modified Tech Stack and Tool Stack

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -3,20 +3,39 @@ import { Col, Row } from "react-bootstrap";
 import { SiCsharp, SiPostgresql, SiMysql, SiAzureartifacts, SiAzuredataexplorer, SiAzuredevops, SiAzurefunctions, SiGraphql, SiMicrosoftsqlserver, SiJavascript, SiGit, SiDotnet } from 'react-icons/si';
 import { FaAws, FaDocker, FaGit, FaJenkins } from 'react-icons/fa';
 import { DiReact, DiHtml5, DiCss3, DiAws, DiJavascript, DiNodejs, DiDotnet } from 'react-icons/di';
+import { SiTerraform, SiAnsible } from 'react-icons/si';
 import { BsBootstrap } from "react-icons/bs";
 import WunderGraphIcon from "./WunderGraphIcon";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons" title=".NET">
-        <SiDotnet />
-      </Col>
       <Col xs={4} md={2} className="tech-icons" title="C#">
         <SiCsharp />
       </Col>
+      <Col xs={4} md={2} className="tech-icons" title="React">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons" title="PostgreSQL">
+        <SiPostgresql />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons" title="Docker">
+        <FaDocker />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons" title="WunderGraph">
+        <WunderGraphIcon />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons" title="Terraform">
+        <SiTerraform />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons" title="Ansible">
+        <SiAnsible />
+      </Col>
       <Col xs={4} md={2} className="tech-icons" title="Javascript">
         <SiJavascript />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons" title="AWS">
+        <FaAws />
       </Col>
       <Col xs={4} md={2} className="tech-icons" title="Git">
         <SiGit />
@@ -24,26 +43,8 @@ function Techstack() {
       <Col xs={4} md={2} className="tech-icons" title="GraphQL">
         <SiGraphql />
       </Col>
-      <Col xs={4} md={2} className="tech-icons" title="WunderGraph">
-        <WunderGraphIcon />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons" title="Docker">
-        <FaDocker />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons" title="Jenkins">
-        <FaJenkins />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons" title="AWS">
-        <FaAws />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons" title="PostgreSQL">
-        <SiPostgresql />
-      </Col>
       <Col xs={4} md={2} className="tech-icons" title="Microsoft SQL Server">
         <SiMicrosoftsqlserver />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons" title="React">
-        <DiReact />
       </Col>
       <Col xs={4} md={2} className="tech-icons" title="HTML5">
         <DiHtml5 />
